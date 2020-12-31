3f16953ef565049f4f5ebc8a6c70cdd4e07ea7cc
Michal Bartosik <msbartosik@gmail.com>
2020-12-31 18:29:21 +0100

    set up project

---
+++ src/test/java/com/codecool/movieaddict/MovieAddictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.movieaddict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAddictApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
